49b455ed9a18940a00752e7b6fd349d77022c484
nullity00 <reshmashaik3011@gmail.com>
2024-12-28 03:33:17 +0400

    minor param tweaks

--- next.config.js
+++ next.config.js
@@ -1,12 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   images: {
     unoptimized: true,
   },
   compiler: {
     styledComponents: true,
-  }
-}
+  },
 
-module.exports = nextConfig
+  exportPathMap: async function () {
+    return {
+      "/": { page: "/" },
+      "/contact-us": { page: "/contact-us" },
+      "/fellowships": { page: "/fellowships" },
+      "/members": { page: "/members" },
+      "/services": { page: "/services" },
+      "/zBlock1": { page: "/zBlock1" },
+      "/zBlock2": { page: "/zBlock2" },
+      "/zk": { page: "/zk" },
+    };
+  },
+};
+
+module.exports = nextConfig;
